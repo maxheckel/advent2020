7e89155eec107cb57fecc08684b64d735c5c8533
Max Heckel <max.heckel@mheducation.com>
2020-12-08 09:57:33 -0500

    Day 8

---
+++ 8/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Operation struct {
+	opType string
+	opValue int
+}
+
+func OperationFromString(input string) *Operation {
+	op := &Operation{}
+	split := strings.Split(input, " ")
+	op.opType = split[0]
+
+	op.opValue, _ = strconv.Atoi(split[1][1:len(split[1])])
+	if strings.Contains(split[1], "-") {
+		op.opValue *= -1
+	}
+	return op
+}
+
+func wasVisited(op *Operation, visited []*Operation) bool{
+	for _, opCheck := range visited {
+		if opCheck == op {
+			return true
+		}
+	}
+	return false
+}
+
+func main(){
+	file, err := os.Open("./8/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	var ops []*Operation
+	for scanner.Scan() {
+		ops = append(ops, OperationFromString(scanner.Text()))
+	}
+	terminated := false
+	var result int
+	result, _ = runOps(ops)
+	fmt.Printf("Part 1: %d\n", result)
+
+	iterator := 0
+	for !terminated {
+		for ops[iterator].opType == "acc" {
+			iterator++
+		}
+		swapOp(ops, iterator)
+		result, terminated = runOps(ops)
+		// Switch it back
+		swapOp(ops, iterator)
+		iterator++
+	}
+	fmt.Printf("Part 2: %d", result)
+
+}
+
+func swapOp(copiedOps []*Operation, position int) {
+	switch copiedOps[position].opType {
+	case "jmp":
+		copiedOps[position].opType = "nop"
+		break
+	case "nop":
+		copiedOps[position].opType = "jmp"
+	}
+}
+
+func runOps(ops []*Operation) (int, bool) {
+	currentVal := 0
+	currentPos := 0
+	var visited []*Operation
+	for !wasVisited(ops[currentPos], visited) {
+		visited = append(visited, ops[currentPos])
+		switch ops[currentPos].opType {
+		case "nop":
+			currentPos++
+			break
+		case "acc":
+			currentVal += ops[currentPos].opValue
+			currentPos++
+			break
+		case "jmp":
+			currentPos += ops[currentPos].opValue
+		}
+		// Next instruction terminates
+		if currentPos == len(ops) {
+			return currentVal, true
+		}
+	}
+	return currentVal, false
+}
